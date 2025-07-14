99dbf88088e28b431373d73c9484df2fc2afeea7
AlberteMB <albertebardanca@hotmail.com>
2025-07-14 17:07:43 +0200

    Solving issues in POM and adding method in CustomerController

--- businessdomain/customer/src/main/java/com/paymentchain/controller/CustomerRestController.java
+++ businessdomain/customer/src/main/java/com/paymentchain/controller/CustomerRestController.java
@@ -1,12 +1,27 @@
 package com.paymentchain.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.paymentchain.entities.Customer;
+import com.paymentchain.entities.CustomerProduct;
 import com.paymentchain.repository.CustomerRepository;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import java.time.Duration;
+import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -14,6 +29,27 @@ public class CustomerRestController {
 
     @Autowired
     private CustomerRepository customerRepository;
+    
+    private final WebClient.Builder webClientBuilder;
+    
+    public CustomerRestController (WebClient.Builder webClientBuilder){
+        this.webClientBuilder = webClientBuilder;
+    }
+    
+    HttpClient client = HttpClient.create()
+            // Connection Timeout: is a period within which a connection between a client and a server must be stablished
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(EpollChannelOption.TCP_KEEPIDLE, 300)
+            .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+            // Response Timeout: The maximum time we wait to receive a response after sending a request
+            .responseTimeout(Duration.ofSeconds(1))
+            // Read and Write Timeout: A read timeout occurs when no data was read within a certain
+            //period of time, while the write timeout when a write operation cannot finish at an specific time
+            .doOnConnected(connection -> {
+              connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+              connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+            });
 
     // Create - POST
     @PostMapping
@@ -76,5 +112,31 @@ public class CustomerRestController {
                     return ResponseEntity.ok().build();
                 })
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    
+    @GetMapping("/full")
+    public Customer getByCode (@RequestParam String code) {
+        Customer customer = customerRepository.findByCode(code);
+        List<CustomerProduct> products = customer.getProducts();
+        products.forEach(x -> {
+            String productName = getProductName(x.getId());
+            x.setProductName(productName);
+        });
+        return customer;
+                
+    }
+    
+    private String getProductName (long id){
+        WebClient build =webClientBuilder.clientConnector(new ReactorClientHttpConnector(client))
+                .baseUrl("http://localhost:8082/product")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8082/product"))
+                .build();
+        
+        JsonNode block = build.method(HttpMethod.GET).uri("/" + id)
+                .retrieve().bodyToMono(JsonNode.class).block();
+        
+        String name = block.get("name").asText();
+        return name;
     }
 }
